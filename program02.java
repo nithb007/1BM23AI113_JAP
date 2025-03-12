c60ef03886ad52ab295f16f1cf0183ebe4b3274b
nithb007 <nitheeshb.ai23@bmsce.ac.in>
2025-03-12 20:33:58 +0530

    Add files via upload

---
+++ program02.java
@@ -0,0 +1,69 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class program02 {
+    public static void main(String[] args) {
+        int a=5;
+        Integer b=a;
+        int c=b ;
+        System.out.println("INTEGER");
+        System.out.println("Value of a = "+a);
+        System.out.println("Value of b = "+b);
+        System.out.println("Value of c = "+c);
+
+        double x=5.0;
+        Double y=x;
+        double z=y ;
+        System.out.println("DOUBLE");
+        System.out.println("Value of x = "+x);
+        System.out.println("Value of y = "+y);
+        System.out.println("Value of z = "+z);
+
+        byte x1=5;
+        Byte y1=x1;
+        byte z1=y1 ;
+        System.out.println("BYTE");
+        System.out.println("Value of x1 = "+x1);
+        System.out.println("Value of y1 = "+y1);
+        System.out.println("Value of z1 = "+z1);
+
+        char x2='a';
+        Character y2=x2;
+        char z2=y2 ;
+        System.out.println("CHARACTER");
+        System.out.println("Value of x2 = "+x2);
+        System.out.println("Value of y2 = "+y2);
+        System.out.println("Value of z2 = "+z2);
+
+        boolean x3=true;
+        Boolean y3=x3;
+        boolean z3=y3 ;
+        System.out.println("BOOLEAN");
+        System.out.println("Value of x3= "+x3);
+        System.out.println("Value of y3 = "+y3);
+        System.out.println("Value of z3 = "+z3);
+
+        float x4 = 5.00F;
+        Float y4 = x4;
+        float z4 = y4;
+        System.out.println("FLOAT");
+        System.out.println("Value of x4 = "+ x4);
+        System.out.println("Value of y6 = "+ y4);
+        System.out.println("Value of z6 = "+ z4);
+
+        long x5 = 456L;
+        Long y5 = x5;
+        long z5 = y5;
+        System.out.println("LONG");
+        System.out.println("Value of x5 = "+ x5);
+        System.out.println("Value of y5 = "+ y5);
+        System.out.println("Value of z5 = "+ z5);
+
+        short x6 = 456;
+        Short y6 = x6;
+        short z6 = y6;
+        System.out.println("SHORT");
+        System.out.println("Value of x6 = "+ x6);
+        System.out.println("Value of y6 = "+ y6);
+        System.out.println("Value of z6 = "+ z6);
+    }
+}
